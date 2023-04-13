43228bb248c43a886eb9f8ce7323c5d5af206d27
Carlmelo Booc <car1melo@hotmail.com>
2023-04-13 13:28:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Rene is the champion"
